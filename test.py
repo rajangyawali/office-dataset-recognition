41f675a900e561b76da24d4ba2a88f357f2295c5
rajangyawali <rajangyawali93@gmail.com>
2019-05-05 10:05:44 +0545

    Test Dataset Updated

--- test.py
+++ test.py
@@ -260,10 +260,11 @@ if __name__ == '__main__':
 			test_label = [1,0,0,0]
 		elif word_label == 'rajan': 
 			test_label = [0,1,0,0]
-    	elif word_label == 'rupesh': 
-			test_label = [0,0,1,0]
+    	
     	elif word_label == 'sakar': 
 			test_label = [0,0,0,1]
+		else:
+			test_label = [0,0,1,0]
 		test_score.append(test_label)
 		total_tic = time.time()
 		im_file = os.path.join(current_dir, image)
